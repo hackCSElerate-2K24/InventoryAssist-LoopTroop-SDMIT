da1c7dec3c1af8be70a634b8a2dd12352a2e19f5
Subodh Pataki <135350282+PatakiSubodh@users.noreply.github.com>
2024-11-14 16:07:52 +0530

    sidebar init

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
   )
 }
